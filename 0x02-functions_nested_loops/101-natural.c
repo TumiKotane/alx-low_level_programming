83a95354b4fed3e06eab76d8f363cc554f38213c
TumiKotane <kaiineoyoung@gmail.com>
2023-02-22 10:00:09 -0800

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,16 +5,13 @@
  */
 int main(void)
 {
-	int i, z = 0;
+	int i, sum = 0;
 
-	while (i < 1024)
+	for (i = 0; i < 1024; i++)
 	{
-	if ((i % 3 == 0) || (i % 5 == 0))
-	{
-	z += i;
+	if ((i % 3) == 0 || (i % 5) == 0)
+	sum += i;
 	}
-	i++
-	}
-	printf("%d\n", z);
+	printf("%d\n", sum);
 	return (0);
 }
