52c1a6e39e5f04ef79c581e6e863da32e0cfb0ce
TumiKotane <kaiineoyoung@gmail.com>
2023-02-27 10:14:50 -0800

    8-print_array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,7 @@
 /**
  * print_array - prints n elements of an array of integers
  * @a: array of integers
- * @a: number of elements of the array to be printed
+ * @n: number of elements of the array to be printed
  * Return: void
  */
 void print_array(int *a, int n)
