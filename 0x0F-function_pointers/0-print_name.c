d7ef502390b5ab50484efa1a6f3ed2d29494bebe
TumiKotane <kaiineoyoung@gmail.com>
2023-06-21 04:02:32 -0700

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,7 +6,7 @@
  * @f: pointer to function
  * Return: nothing
  */
-void print_name(char *name, void (*f)(char 8))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 	return;
